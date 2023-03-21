7a236eb58a29651c2b56c8f4a7a16e2db4879579
Linux Ubiquitous <lubi888@users.noreply.github.com>
2023-03-21 02:28:51 +0000

    Update gosync_text.dart

--- lib/gosync_text.dart
+++ lib/gosync_text.dart
@@ -1,4 +1,5 @@
-const String goSyncTitle = "ethSync.go 20230316 Paddys day pixel 4a";
+const String goSyncTitle =
+    "ethSync.go 20230320 Paddys day weekend pixel 4a Win mobi app";
 
 const String ethSyncPrimaryGoals =
     ('1, The primary goal of this mobile app is to educate the reader about golang installation.\n\n'
