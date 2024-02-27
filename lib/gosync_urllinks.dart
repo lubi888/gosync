8e31387ecdc67670808efa2f9a9838a308e95afa
Linux Ubiquitous <lubi888@users.noreply.github.com>
2024-02-27 16:01:33 +0000

    updates

--- lib/gosync_urllinks.dart
+++ lib/gosync_urllinks.dart
@@ -1,242 +1,242 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
-// import './gosync_scrollbar1.dart';
-// import './gosync_text.dart';
-// import 'dart:html';
-
 // import 'package:flutter/material.dart';
-// import 'package:async/async.dart';
-// import 'dart:html';
+// import 'dart:async';
+// // import './gosync_scrollbar1.dart';
+// // import './gosync_text.dart';
+// // import 'dart:html';
 
-import 'package:url_launcher/url_launcher.dart';
-// import 'package:flutter_web_browser/flutter_web_browser.dart';
+// // import 'package:flutter/material.dart';
+// // import 'package:async/async.dart';
+// // import 'dart:html';
 
-// final Uri _url = Uri.parse('https://flutter.dev');
-// final Uri _urlEthereumOrgLightChain =
+// import 'package:url_launcher/url_launcher.dart';
+// // import 'package:flutter_web_browser/flutter_web_browser.dart';
+
+// // final Uri _url = Uri.parse('https://flutter.dev');
+// // final Uri _urlEthereumOrgLightChain =
+// //     Uri.parse('https://ethereum.org/en/developers/docs/nodes-and-clients/');
+
+// // https://ethereum.org/en/developers/docs/nodes-and-clients/
+
+// // Future<void> _launchUrl() async {
+// //   if (!await launchUrl(_url)) {
+// //     throw "could not luanch $_url";
+// //   }
+// // }
+
+// // final Uri _url = Uri.parse('https://flutter.dev');
+// // new links 2024.01
+// final Uri _urlGolangDL = Uri.parse('https://go.dev/dl');
+// // final Uri _urlGolangDL = Uri.parse('https://www.golang.org/dl');
+
+// void _launchGolangDLUrl() {}
+// Future<void> _launchGolangDLUrl() async {
+//   debugPrint("customer left app to golang.org/dl at");
+//   print(TimeOfDay.now());
+//   if (!await launchUrl(_urlGolangDL)) {
+//     throw Exception('Could not launch $_urlGolangDL');
+//   }
+// }
+
+// // final Uri _uriGolangDL = Uri.https("go.dev", "dl");
+// final Uri _uriGolangMain = Uri.https("go.dev");
+// final Uri _launchLightChainSync =
 //     Uri.parse('https://ethereum.org/en/developers/docs/nodes-and-clients/');
 
-// https://ethereum.org/en/developers/docs/nodes-and-clients/
+// // Future<void> _launchGolangDLUrl() async {
+// //   print("customer left app to go.dev/dl at");
+// //   print(TimeOfDay.now());
+// //   if (!await launchUrl(_uriGolangDL)) {
+// //     throw "could not launch $_launchGolangDL";
+// //   }
+// // }
 
-// Future<void> _launchUrl() async {
-//   if (!await launchUrl(_url)) {
-//     throw "could not luanch $_url";
-//   }
-// }
-
-// final Uri _url = Uri.parse('https://flutter.dev');
-// new links 2024.01
-final Uri _urlGolangDL = Uri.parse('https://go.dev/dl');
-// final Uri _urlGolangDL = Uri.parse('https://www.golang.org/dl');
-
-void _launchGolangDLUrl() {}
-Future<void> _launchGolangDLUrl() async {
-  debugPrint("customer left app to golang.org/dl at");
-  print(TimeOfDay.now());
-  if (!await launchUrl(_urlGolangDL)) {
-    throw Exception('Could not launch $_urlGolangDL');
-  }
-}
-
-// final Uri _uriGolangDL = Uri.https("go.dev", "dl");
-final Uri _uriGolangMain = Uri.https("go.dev");
-final Uri _launchLightChainSync =
-    Uri.parse('https://ethereum.org/en/developers/docs/nodes-and-clients/');
-
-// Future<void> _launchGolangDLUrl() async {
-//   print("customer left app to go.dev/dl at");
+// Future<void> _launchGolangMainWebsite() async {
+//   debugPrint("customer left app to go.dev at");
 //   print(TimeOfDay.now());
-//   if (!await launchUrl(_uriGolangDL)) {
-//     throw "could not launch $_launchGolangDL";
+//   if (!await launchUrl(_uriGolangMain)) {
+//     throw "could not luanch $_uriGolangMain";
 //   }
 // }
 
-Future<void> _launchGolangMainWebsite() async {
-  debugPrint("customer left app to go.dev at");
-  print(TimeOfDay.now());
-  if (!await launchUrl(_uriGolangMain)) {
-    throw "could not luanch $_uriGolangMain";
-  }
-}
+// Future<void> _ethLightChainSync() async {
+//   print("customer left app to eth.dev at");
+//   print(TimeOfDay.now());
+//   if (!await launchUrl(_launchLightChainSync)) {
+//     throw "could not launch $_ethLightChainSync";
+//   }
+// }
 
-Future<void> _ethLightChainSync() async {
-  print("customer left app to eth.dev at");
-  print(TimeOfDay.now());
-  if (!await launchUrl(_launchLightChainSync)) {
-    throw "could not launch $_ethLightChainSync";
-  }
-}
-
-// final Uri _url = Uri.parse('https://flutter.dev');
-// new links 2024.01
-final Uri _urlGolangDL = Uri.parse('https://go.dev/dl');
-// final Uri _urlGolangDL = Uri.parse('https://www.golang.org/dl');
-Future<void> _launchGolangDLUrl() async {
-  debugPrint("customer left app to golang.org/dl at");
-  print(TimeOfDay.now());
-  if (!await launchUrl(_urlGolangDL)) {
-    throw Exception('Could not launch $_urlGolangDL');
-  }
-}
-
+// // final Uri _url = Uri.parse('https://flutter.dev');
+// // new links 2024.01
+// final Uri _urlGolangDL = Uri.parse('https://go.dev/dl');
+// // final Uri _urlGolangDL = Uri.parse('https://www.golang.org/dl');
 // Future<void> _launchGolangDLUrl() async {
-//   if (!await launchUrl(_url)) {
-//     throw Exception('Could not launch $_url');
+//   debugPrint("customer left app to golang.org/dl at");
+//   print(TimeOfDay.now());
+//   if (!await launchUrl(_urlGolangDL)) {
+//     throw Exception('Could not launch $_urlGolangDL');
 //   }
 // }
 
-class EthSyncLaunchUrl {
-  static void launchURLFlutter() async {
-    const url = "https://flutter.io";
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
+// // Future<void> _launchGolangDLUrl() async {
+// //   if (!await launchUrl(_url)) {
+// //     throw Exception('Could not launch $_url');
+// //   }
+// // }
 
-  static launchURLGithub() async {
-    const url = "https://github.com";
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  static launchURLEthStackexchange() async {
-    const url = "https://ethereum.stackexchange.com/";
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  static launchURLEthYouTube() async {
-    const url =
-        "https://www.youtube.com/channel/UCNOfzGXD_C9YMYmnefmPH0g/featured";
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  // https://ethereum.stackexchange.com/
-
-// void _launchURLX(String x_url) async {
-//   const url= x_url;
-//   if (await canLaunch(url)) {
-//     await launch(url);
-//   } else {//
-//     throw "could not launch $url";
+// class EthSyncLaunchUrl {
+//   static void launchURLFlutter() async {
+//     const url = "https://flutter.io";
+//     if (await canLaunch(url)) {
+//       await launch(url);
+//     } else {
+//       throw 'Could not launch $url';
+//     }
 //   }
+
+//   static launchURLGithub() async {
+//     const url = "https://github.com";
+//     if (await canLaunch(url)) {
+//       await launch(url);
+//     } else {
+//       throw 'Could not launch $url';
+//     }
+//   }
+
+//   static launchURLEthStackexchange() async {
+//     const url = "https://ethereum.stackexchange.com/";
+//     if (await canLaunch(url)) {
+//       await launch(url);
+//     } else {
+//       throw 'Could not launch $url';
+//     }
+//   }
+
+//   static launchURLEthYouTube() async {
+//     const url =
+//         "https://www.youtube.com/channel/UCNOfzGXD_C9YMYmnefmPH0g/featured";
+//     if (await canLaunch(url)) {
+//       await launch(url);
+//     } else {
+//       throw 'Could not launch $url';
+//     }
+//   }
+
+//   // https://ethereum.stackexchange.com/
+
+// // void _launchURLX(String x_url) async {
+// //   const url= x_url;
+// //   if (await canLaunch(url)) {
+// //     await launch(url);
+// //   } else {//
+// //     throw "could not launch $url";
+// //   }
+// // }
+//   static launchURLEthSync() async {
+//     const url = 'https://www.ethsync.xxx';
+//     if (await canLaunch(url)) {
+//       await launch(url);
+//     } else {
+//       throw 'Could not launch $url';
+//     }
+//   }
+
+//   static launchURLEthereum() async {
+//     const url = 'https://ethereum.org/';
+//     if (await canLaunch(url)) {
+//       await launch(url);
+//     } else {
+//       throw 'Could not launch $url';
+//     }
+//   }
+
+//   // static launchBrowserTabKrakenEthEur() async {
+//   //   await FlutterWebBrowser.openWebPage(
+//   //       // url: "https://flutter.io/", androidToolbarColor: Colors.red[900]);
+//   //       url: "https://trade.kraken.com/markets/kraken/eth/eur");
+//   //   // androidToolbarColor: Colors.red);
+//   // }
+
+// //  void _onLubiEthWeb() async {
+// //    const url = 'https://linuxubiuitous.com/ethsync';
+// //    if (await canLaunch(url)) {
+// //      await launch(url);
+// //    } else {
+// //      throw 'Could not launch $url';
+// //    }
+// //  }
+
+//   // void _shareEthSync() {
+//   //   final RenderBox box = context.findRenderObject();
+//   //   Share.share("www.et.eu",
+//   //       sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
+//   // }
+
+//   // final String url_cry_malware = "https://coinpedia.org/news/firefox-upgrades-to-ban-crypto-mining-malware/";
+//   // visit golang.org/dl
+//   // class EthSyncUrl {
+//   static void launchURLGolangOrgDL() async {
+//     const url = "https://golang.org/dl";
+//     if (await canLaunch(url)) {
+//       await launch(url);
+//     } else {
+//       throw 'Could not launch $url';
+//     }
+//   }
+
+//   static void launchURLGolangOrgInstall() async {
+//     const url = "https://golang.org/doc/install";
+//     if (await canLaunch(url)) {
+//       await launch(url);
+//     } else {
+//       throw 'Could not launch $url';
+//     }
+//   }
+
+//   static void launchURLPocketGophers() async {
+//     const url = "https://pocketgophers.com/go-install-vs-go-build/";
+//     if (await canLaunch(url)) {
+//       await launch(url);
+//     } else {
+//       throw 'Could not launch $url';
+//     }
+//   }
+
+//   static void launchURLGethWeb() async {
+//     const url = "https://geth.ethereum.org";
+//     if (await canLaunch(url)) {
+//       await launch(url);
+//     } else {
+//       throw 'Could not launch $url';
+//     }
+//   }
+
+//   static void launchURLGethWebDL() async {
+//     const url = "https://geth.ethereum.org/downloads/";
+//     if (await canLaunch(url)) {
+//       await launch(url);
+//     } else {
+//       throw 'Could not launch $url';
+//     }
+//   }
+
+//   static void launchURLGethWebInstall() async {
+//     const url = "https://geth.ethereum.org/install-and-build/Installing-Geth";
+//     if (await canLaunch(url)) {
+//       await launch(url);
+//     } else {
+//       throw 'Could not launch $url';
+//     }
+//   }
+
+//   static launchURLEtherscanIoChartPrice() async {
+//     const url = "https://etherscan.io/chart/etherprice";
+//     if (await canLaunch(url)) {
+//       await launch(url);
+//     } else {
+//       throw 'Could not launch $url';
+//     }
+//   }
+
+//   // https://geth.ethereum.org/downloads/
 // }
-  static launchURLEthSync() async {
-    const url = 'https://www.ethsync.xxx';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  static launchURLEthereum() async {
-    const url = 'https://ethereum.org/';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  // static launchBrowserTabKrakenEthEur() async {
-  //   await FlutterWebBrowser.openWebPage(
-  //       // url: "https://flutter.io/", androidToolbarColor: Colors.red[900]);
-  //       url: "https://trade.kraken.com/markets/kraken/eth/eur");
-  //   // androidToolbarColor: Colors.red);
-  // }
-
-//  void _onLubiEthWeb() async {
-//    const url = 'https://linuxubiuitous.com/ethsync';
-//    if (await canLaunch(url)) {
-//      await launch(url);
-//    } else {
-//      throw 'Could not launch $url';
-//    }
-//  }
-
-  // void _shareEthSync() {
-  //   final RenderBox box = context.findRenderObject();
-  //   Share.share("www.et.eu",
-  //       sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
-  // }
-
-  // final String url_cry_malware = "https://coinpedia.org/news/firefox-upgrades-to-ban-crypto-mining-malware/";
-  // visit golang.org/dl
-  // class EthSyncUrl {
-  static void launchURLGolangOrgDL() async {
-    const url = "https://golang.org/dl";
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  static void launchURLGolangOrgInstall() async {
-    const url = "https://golang.org/doc/install";
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  static void launchURLPocketGophers() async {
-    const url = "https://pocketgophers.com/go-install-vs-go-build/";
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  static void launchURLGethWeb() async {
-    const url = "https://geth.ethereum.org";
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  static void launchURLGethWebDL() async {
-    const url = "https://geth.ethereum.org/downloads/";
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  static void launchURLGethWebInstall() async {
-    const url = "https://geth.ethereum.org/install-and-build/Installing-Geth";
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  static launchURLEtherscanIoChartPrice() async {
-    const url = "https://etherscan.io/chart/etherprice";
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  // https://geth.ethereum.org/downloads/
-}
